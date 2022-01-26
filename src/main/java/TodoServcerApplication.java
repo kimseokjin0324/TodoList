7d1e52a20791078a7d2126752a0ec5841ad70630
kimseokjin <kimseokjin0324@gmail.com>
2022-01-27 02:18:06 +0900

    Initial commit

---
+++ src/main/java/TodoServcerApplication.java
@@ -0,0 +1,5 @@
+public class TodoServcerApplication {
+    public static void main(String[] args) {
+        System.out.println("HELLO TODO");
+    }
+}
---
+++ src/main/java/model/TodoEntity.java
@@ -0,0 +1,24 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor          //- 어노테이션은 파라미터가 없는 기본생성자 생성
+@AllArgsConstructor         //- 어노테이션은 모든 필드 값을 파라미터로 받는 생성자를 만들어줌
+public class TodoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     //-자동생성하고 Identity : 데이터베이스에 위임
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(name = "todoOrder",nullable = false)
+    private Long order;
+    @Column(nullable = false)
+    private Boolean completed;
+}
